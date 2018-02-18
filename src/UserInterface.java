e55d3fc49b19f8bbb8c0d6c8df02f85096b46874
Benjamin <benjaminpalko@hotmail.com>
2018-02-17 23:22:01 -0500

    Commiting

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -92,12 +92,15 @@ public class UserInterface {
 		switch (tabPane.getSelectedIndex())
 		{
 		case 0:
+			client.takeInput(s);
 			client.getOutputWindow().append(s + "\n");
 			break;
 		case 1:
+			errorSim.takeInput(s);
 			errorSim.getOutputWindow().append(s + "\n");
 			break;
 		case 2:
+			server.takeInput(s);
 			server.getOutputWindow().append(s + "\n");
 			break;
 		}
